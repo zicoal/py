1967cdcd88fbf745aa17a936034d7b18011e6d98
zico <jianjinshu@126.com>
2018-12-19 15:12:52 +0800

    win_20181015

---
+++ cn/edu/hznu/nos/InterCitationWithYear.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+# coding:utf-8
+
+import json
+import os
+import logging
+import time
+
+
+
+time_start=time.time()
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+# 定义handler的输出格式
+#logger to console
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+#pc
+#src_dir='/home/zico/py/data/'
+#src_root_file='/home/zico/py/discipline.txt'
+#dest_dir='/home/zico/py/data/processed/'
+#dest_file_papers=dest_dir+"papers.txt"
+
+#server
+#src_dir='/home/zico/mag/data/microsoft-2017-nov/'
+src_root_file='/home/zico/mag/data/discipline.txt'
+src_dir='/home/zico/mag/data/processed/'
+src_file_papers=src_dir+"papers.txt"
+dest_file_interdispline_citation= src_dir+"inter_citation_year.txt"
+dest_file_discipline_papers_nums= src_dir+"discipline_papers_year.txt"
+
+
+#reeading papers
+f = open(src_file_papers,encoding='UTF-8', mode='r',errors='ignore')
+line =f.readline()
+num_totallines=0
+dict_paper_discipline= {}
+
+dict_disciplines= {}
+num_no_disciplines=0
+
+while line:
+    num_totallines += 1
+    if(num_totallines%2!=0):
+        #paper line (odd line)
+        words = line.split('\t')
+        p_id= words[0].strip()
+        p_year = words[1].strip()
+        p_discipline = words[2].strip()
+        if(len(p_discipline)==0):
+            num_no_disciplines += 1
+            line = f.readline()
+            continue
+        if (dict_disciplines.get(p_discipline) is None):
+            dict_disciplines.setdefault(p_discipline, 1)
+        else:
+            dict_disciplines[p_discipline] += 1
+
+        dict_paper_discipline.setdefault(p_id, p_discipline)
+
+    line =f.readline()
+    if (num_totallines % 1000000 == 0):
+        time_end = time.time()
+        logger.info('Reading papers: %d, cost time:%d s', num_totallines, time_end - time_start)
+f.close()
+
+time_end = time.time()
+logger.info('Read Papers Done, #total papers: %d, cost time:%d s',num_totallines/2, time_end - time_start)
+
+#reeading citation
+f = open(src_file_papers,encoding='UTF-8', mode='r',errors='ignore')
+line =f.readline()
+num_linecount=0
+dict_discipline_ctations= {}
+p_discipline = ''
+b_valid_label=False
+while line:
+    num_linecount += 1
+    if(num_linecount%2!=0):
+        #paper line (odd line)
+        words = line.split('\t')
+#        if(len(words)<3):
+#            print(line)
+#            print(num_linecount)
+        p_discipline = words[2].strip()
+        if (len(p_discipline) == 0):
+            p_discipline = ''
+            b_valid_label = False
+        else:
+            b_valid_label = True
+    elif(num_linecount%2==0 and b_valid_label == True):
+        #citation line (even line)
+        refs_tmp = line.replace('[', '').replace(']', '').replace('\n', '')
+        if (len(refs_tmp.strip()) == 0):
+            line = f.readline()
+            continue
+        refs = refs_tmp.split(',')
+        for ref in refs:
+            if(len(ref.strip())==0):
+                continue
+            if dict_paper_discipline.get(ref.strip()) is not None:
+                ref_discipline = dict_paper_discipline.get(ref.strip())
+                keys =dict_discipline_ctations.get(ref_discipline)
+
+                if(keys is None):
+                    # no citing paper
+                    dict_discipline_ctations.setdefault(ref_discipline,{})[p_discipline]=1
+                elif (p_discipline in keys):
+                    dict_discipline_ctations[ref_discipline][p_discipline] +=1
+
+                    '''
+                    print(keys)
+                    print(refs)
+                    print(ref.strip() + ":" + ref_discipline)
+                    print(p_discipline + ":" + p_discipline)
+                    print(keys)
+                    print(dict_discipline_ctations[ref_discipline][p_discipline])
+                    exit(0)
+
+                    if ( dict_discipline_ctations[ref_discipline][p_discipline] >10000000):
+                        print(ref_discipline +":" +p_discipline)
+                        exit(0)
+                    '''
+                else:
+                    #citing paper does not exist
+                    dict_discipline_ctations.setdefault(ref_discipline,{})[p_discipline]=1
+    line =f.readline()
+    if (num_linecount % 1000000 == 0 or num_linecount==num_totallines):
+        time_end = time.time()
+        logger.info('Reading citations : %d, process:%f%%,num_no_disciplines:%d, cost time:%d s', num_linecount, num_linecount*100.0/num_totallines, num_no_disciplines, time_end - time_start)
+f.close()
+
+str_interdiscipline=""
+for keys,dict_discipline_ctations_values in dict_discipline_ctations.items():
+    for key, value in dict_discipline_ctations_values.items():
+        #a<-b
+        str_interdiscipline = str_interdiscipline+("%s\t%s\t%d\n" % (keys,key,value))
+
+f_citations = open(dest_file_interdispline_citation, encoding='UTF-8', mode='w', errors='ignore')
+f_citations.write(str_interdiscipline)
+f_citations.close()
+
+str_discipline_papers=""
+for  key, value  in dict_disciplines.items():
+        #a<-b
+        str_discipline_papers = str_discipline_papers+("%s\t%d\n" % (key,value))
+
+f_papers = open(dest_file_discipline_papers_nums, encoding='UTF-8', mode='w', errors='ignore')
+f_papers.write(str_discipline_papers)
+f_papers.close()
+
+
+time_end = time.time()
+logger.info(' Done, #total lines: %d,#total papers: %d,#empty_disciplines: %d, cost time:%d s',num_totallines/2,num_totallines/2-num_no_disciplines, num_no_disciplines, time_end - time_start)
--- cn/edu/hznu/nos/output/plotnetworkx.py
+++ cn/edu/hznu/nos/output/plotnetworkx.py
@@ -6,8 +6,11 @@ import json
 import os
 import logging
 import time
+import random
 from pyecharts import Graph
 from pyecharts import Style
+from pyecharts import Bar
+from pyecharts import Line
 
 style = Style(
     title_color="#000",
@@ -16,9 +19,6 @@ style = Style(
     height=800,
 #    background_color='#404a59'
 )
-
-
-
 
 time_start=time.time()
 
@@ -35,10 +35,14 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 
-src_dir='/home/zico/data/mag/processed/'
+#---win---
+src_dir='D:\\py\\cn\\edu\\hznu\\nos\\data\\'
+
 src_file_papers=src_dir+"discipline_papers.txt"
 src_file_citations=src_dir+"interdispline_citation.txt"
 
+#---linux---
+#src_dir='/home/zico/data/mag/processed/'
 dest_file_papers=src_dir+"normalized_discipline_papers.txt"
 dest_file_citations_citing=src_dir+"normalized_citing_interdispline_citation.txt"
 dest_file_citations_cited=src_dir+"normalized_cited_interdispline_citation.txt"
@@ -74,9 +78,13 @@ dict_disciplines_papers_num = sorted(dict_disciplines_papers_num.items(), key=la
 #for key,value in dict_disciplines_papers_num.items():
 f = open(dest_file_papers, encoding='UTF-8', mode='w', errors='ignore')
 
+papers=[]
+papers_nums=[]
 for p in  dict_disciplines_papers_num:
     f.write('%s\t%s\n' % (p[0],100.0*p[1]/num_max_papers))
     nodes.append({"name":p[0],"symbolSize":100.0*p[1]/num_max_papers})
+    papers.append(p[0])
+    papers_nums.append(p[1])
 
 f.close()
 dict_disciplines_cited= {}
@@ -101,15 +109,79 @@ f.close()
 
 link_width={}
 
+'''
 num_top_citations = 3
 for keys,values in dict_disciplines_citing.items():
+    num_max = 0
+    for key, value in values.items():
+        if(num_max < value):
+            num_max = value
     values = sorted(values.items(), key=lambda x: x[1],reverse=True)
     top = 0
     for key in values:
         if(top<num_top_citations):
-            links.append({"source": keys, "target": key[0]})
             top += 1
+            if(key[0]!=keys):
+                links.append({"source": keys,
+                              "target": key[0],
+                              "lineStyle":{
+                                  "normal":{
+                                      "width": 2*int(key[1])*1.0/num_max,
+                                      #"curveness": 0.2
+                                      "curveness": random.uniform(0.1,0.4)
+                                      }
+                                  }
+                              })
+                print(keys+"->"+key[0]+":"+str(2*int(key[1])*1.0/num_max))
+'''
 
+num_top_citations = 3
+for keys,values in dict_disciplines_citing.items():
+    num_total= 0
+    for key, value in values.items():
+        num_total += value
+    values = sorted(values.items(), key=lambda x: x[1],reverse=True)
+    top = 0
+    for key in values:
+        if(top<num_top_citations):
+            top += 1
+            if(key[0]!=keys):
+                links.append({"source": keys,
+                              "target": key[0],
+                              "lineStyle":{
+                                  "normal":{
+                                      "width": 3*int(key[1])*1.0/num_total,
+                                      #"curveness": 0.2
+                                      "curveness": random.uniform(0.1,0.4)
+                                      }
+                                  }
+                              })
+                print(keys+"->"+key[0]+":"+str(int(key[1])*1.0/num_total))
+
+'''
+links_cited={}
+for keys,values in dict_disciplines_cited.items():
+    num_total= 0
+    for key, value in values.items():
+        num_total += value
+    values = sorted(values.items(), key=lambda x: x[1],reverse=True)
+    top = 0
+    for key in values:
+        if(top<num_top_citations):
+            top += 1
+            if(key[0]!=keys):
+                links_cited.append({"source": key[0],
+                              "target": keys,
+                              "lineStyle":{
+                                  "normal":{
+                                      "width": 3*int(key[1])*1.0/num_total,
+                                      #"curveness": 0.2
+                                      "curveness": random.uniform(0.1,0.4)
+                                      }
+                                  }
+                              })
+                print(keys+"->"+key[0]+":"+str(int(key[1])*1.0/num_total))
+'''
 
 '''
 #output citing
@@ -132,19 +204,43 @@ for keys,values in dict_disciplines_cited.items():
     for key, value in values.items():
         f.write('%s\t%s\t%s\n' % (key,keys,100.0*int(value)*1.0/num_total))
 f.close()
+
+graph_cited = Graph(("InterDiscipline Cited Relationship_%s" % num_top_citations), **style.init_style)
+graph_cited.add(
+    "",
+    nodes,
+    links,
+    is_label_show=True,
+    graph_layout="circular",
+#    line_width =2,
+    label_text_color=None,
+#    line_curve =  0.1,
+    graph_edge_symbol=  [None,"arrow"]
+)
+graph_cited.render("render_cited.html")
 '''
-graph = Graph("InterDiscipline Citing Relationship", **style.init_style)
+graph = Graph(("InterDiscipline Citing Relationship_%s" % num_top_citations), **style.init_style)
 graph.add(
     "",
     nodes,
     links,
     is_label_show=True,
     graph_layout="circular",
-    line_width =2,
+#    line_width =2,
     label_text_color=None,
-    line_curve =  0.1
-#    graph_edge_symbol= "arrow"
+#    line_curve =  0.1,
+    graph_edge_symbol=  [None,"arrow"]
 )
 graph.render()
 
+#---------Paper Distribution-------------------#
+bar = Bar('Paper Distribution','')
+kwargs = dict(
+    name = 'Distribution of Papers',
+    x_axis = papers,
+    y_axis = papers_nums ,
+    xaxis_rotate=45
+)
+bar.add(**kwargs)
+bar.render('papers.html')
 
