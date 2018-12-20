9f669c71d10b03dfe12b03c1bcf44bb441f42d89
zicoal <jianjinshu@126.com>
2018-12-20 14:24:00 +0800

    20181218

--- cn/edu/hznu/nos/output/plot_two_disciplines_year.py
+++ cn/edu/hznu/nos/output/plot_two_disciplines_year.py
@@ -40,8 +40,8 @@ logger.addHandler(ch)
 
 #--linux--
 src_dir='/home/zico/py/cn/edu/hznu/nos/'
-src_file_citations=src_dir+"/data/inter_citation_year.txt"
-dest_fig_citation_patten=src_dir+"/figs/citation_patten_%s_%s_%s_%s_%s.png"
+src_file_citations=src_dir+"data/inter_citation_year.txt"
+dest_fig_citation_patten=src_dir+"figs/citation_patten_%s_%s_%s_%s_%s.png"
 
 colors= ['bisque','lightgreen','slategrey','lightcoral','gold',
          'c','cornflowerblue','blueviolet','tomato','olivedrab',
@@ -61,14 +61,25 @@ num_max_papers_ci=0
 num_linecount=0
 
 dict_year_discipline_citation={}
+dict_disciplines=[]
+
+start_year=1920
+end_year=2017
+
 while line:
         words=line.replace('\n','').split('\t')
 
         str_discipline_cited = words[0].strip()
         str_discipline_citing = words[1].strip()
-
+        if(str_discipline_cited not in dict_disciplines):
+            dict_disciplines.append(str_discipline_cited)
         cite_year =  int(words[2].strip())
         num_citations =  int(words[3].strip())
+
+        if(cite_year<start_year or cite_year>end_year):
+            num_linecount += 1
+            line = f.readline()
+            continue
 
         if (dict_year_discipline_citation.get(cite_year) is None):
             d1_cited_discipline=dict_year_discipline_citation.setdefault(cite_year,{})
@@ -86,44 +97,46 @@ f.close()
 #--unnormalized starts--
 ##output physics->chemistry and physics<-chemistry
 #no normalized
-x1=[]
-y1=[]
-x2=[]
-y2=[]
-start_year=1920
-end_year=2017
-for  keyx, valuex  in dict_year_discipline_citation.items():
-    if(keyx<start_year or keyx>end_year):
-        continue
-    for keyxx, valuexx in valuex.items():
-         if(keyxx==str_observation_discipline1):
-             #physics
-             for keyxxx, valuexxx in valuexx.items():
-                 if (keyxxx == str_observation_discipline2):
-                     x1.append(keyx)
-                     y1.append(valuexxx)
-
-for  keyx, valuex  in dict_year_discipline_citation.items():
-    for keyxx, valuexx in valuex.items():
-         if(keyxx==str_observation_discipline2):
-             #physics
-             for keyxxx, valuexxx in valuexx.items():
-                 if (keyxxx == str_observation_discipline1):
-                     x2.append(keyx)
-                     y2.append(valuexxx)
 
 
-plt.plot(x1, y1, linewidth='2', label=("Physics -> Chemistry"), color=colors[1], linestyle='-', marker='o')
-plt.plot(x2, y2, linewidth='2', label=(" Chemistry-> Physics"), color=colors[2], linestyle='-', marker='^')
-plt.legend(loc='upper left')
-plt.title('Citing Pattern (Unnormalized )',size ='30')
-plt.xlabel('Year',size ='30')
-plt.ylabel('# of Citations',size ='30')
-tmp_dest_fig_citation_patten=dest_fig_citation_patten % ('unnormalized',tmp_citing_displine,tmp_cited_displine.stat_year,end_year);
-plt.savefig(tmp_dest_fig_citation_patten)
+
+for tmp_citing_displine in dict_disciplines:
+    for tmp_cited_displine in dict_disciplines:
+        x1 = []
+        y1 = []
+        x2 = []
+        y2 = []
+        if (tmp_cited_displine == tmp_citing_displine):
+            continue
+        for  keyx, valuex  in dict_year_discipline_citation.items():
+            for keyxx, valuexx in valuex.items():
+                 if(keyxx==tmp_citing_displine):
+                     #physics
+                     for keyxxx, valuexxx in valuexx.items():
+                         if (keyxxx == str_observation_discipline2):
+                             x1.append(keyx)
+                             y1.append(valuexxx)
+
+        for  keyx, valuex  in dict_year_discipline_citation.items():
+            for keyxx, valuexx in valuex.items():
+                 if(keyxx==tmp_cited_displine):
+                     #physics
+                     for keyxxx, valuexxx in valuexx.items():
+                         if (keyxxx == tmp_citing_displine):
+                             x2.append(keyx)
+                             y2.append(valuexxx)
+
+        plt.plot(x1, y1, linewidth='2', label=("%s -> %s",(tmp_citing_displine.capitalize(),tmp_cited_displine.capitalize())), color=colors[1], linestyle='-', marker='o')
+        plt.plot(x2, y2, linewidth='2', label=("%s -> %s",(tmp_cited_displine.capitalize(),tmp_citing_displine.capitalize())), color=colors[2], linestyle='-', marker='^')
+        plt.legend(loc='upper left')
+        plt.title('Citing Pattern (Unnormalized)',size ='30')
+        plt.xlabel('Year',size ='30')
+        plt.ylabel('# of Citations',size ='30')
+        tmp_dest_fig_citation_patten=dest_fig_citation_patten % ('unnormalized',tmp_citing_displine,tmp_cited_displine.stat_year,end_year);
+        plt.savefig(tmp_dest_fig_citation_patten)
+        time_end = time.time()
+        logger.info('Plotting %s - %s (UN_Normalized), cost time:%d s', tmp_citing_displine, tmp_cited_displine,time_end - time_start)
 #plt.show()
-time_end = time.time()
-logger.info('Plotting Physics - Chemistry (UN_Normalized), cost time:%d s', time_end - time_start)
 #--unnormalized ends--
 '''
 ##output physics->chemistry and physics<-chemistry
