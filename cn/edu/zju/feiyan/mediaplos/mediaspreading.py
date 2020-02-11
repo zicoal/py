8779012afd9c867e04be228c769a278b6489424b
zico <jianjinshu@126.com>
2020-02-12 01:12:21 +0800

    win_20200203_feiyan

--- cn/edu/zju/feiyan/mediaplos/mediaspreading.py
+++ cn/edu/zju/feiyan/mediaplos/mediaspreading.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # coding:utf-8
-
+#COVID-19 spreading
 import  pandas  as pd
 import  sys,os
 import logging
@@ -26,23 +26,72 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 data_dir="d://BaiduNetdiskDownload//肺炎相关数据//results//报道-日期.txt"
-
+fig="..//figs//meida_spreading_sir_%s"
 def formatnum(x, pos):
     return '$%f$' % (x/10000000)
 #formatter = FuncFormatter(formatnum)
 #ax2.yaxis.set_major_formatter(formatter)
 
-N = 1000 #N个节点，默认标号为0到N-1
-m0 = 2
-MAX_Spreading_Time=200
-num_initial_infected=int(N*0.02)
-beta =0.1
-mu=0.1
+
+
+dates=[]
+cases=[]
+news=[]
+f = open(data_dir, encoding='UTF-8', mode='r', errors='ignore')
+line = f.readline()
+line_count = 0
+valid_date=16
+while line:
+    if (line_count <valid_date):
+        line_count += 1
+        line = f.readline()
+        continue
+    words = line.replace('号', '').replace('\n', '').split('\t')
+#    logger.info(words)
+    dates.append(int(words[0]))
+    #news.append(int(words[1]))
+    news.append(math.log10(int(words[1])))
+    if (len(words[4])==0):
+        words[4]=0
+    cases.append(int(words[4]))
+    line_count += 1
+    line = f.readline()
+f.close()
+
+alpha=0.2
+lamda=1
+def get_media_impact(t):
+    theta = 0.0
+    tmp_count=t #t starts from 0
+    if t==0:
+        theta = lamda * news[t]
+    else:
+        for i in range(t-1,-1,-1):
+            theta += math.pow(1-lamda,t-i)* news[i]
+        theta = lamda * (news[t] + theta)
+    #logger.info(math.exp(-1*theta*alpha))
+    #sys.exit()
+    return math.exp(-1*theta*alpha)
+
+N = 1000000 #N个节点，默认标号为0到N-1
+m0 = 2  # m0*2 equals average degree
+MAX_Spreading_Time=len(news)
+num_initial_infected=20
+#num_initial_infected=int(N*0.02)
+beta =0.35
+mu=1
 
 logger.info("Generating network...")
 
 g = nx.barabasi_albert_graph(N, m0)
-ps = nx.spring_layout(g)   # 布置框架
+degrees = g.degree()
+degree=[]
+#for d in range(0,N):
+#    degree.append(g.degree(d))
+#print(degrees)
+#print(degree)
+#print("average/sum:%s/%s" % (np.mean(degree),np.sum(degree)))
+#sys.exit()
 
 #for i in range(0, N):
 #    logger.info("%s: %s" % (i,list(G.neighbors(i))))
@@ -81,14 +130,16 @@ x=[]
 S=[]
 I=[]
 R=[]
+IR=[]
 x.append(tmp_spread_count)
 I.append(len(list_infected))
 S.append(len(list_suspective))
 R.append(0)
-while (tmp_spread_count < MAX_Spreading_Time):
+IR.append(len(list_infected))
+while (tmp_spread_count <= MAX_Spreading_Time):
     for infected in list_infected:
         for nb in  list(g.neighbors(infected)):
-            if (nb in list_suspective and random.random() <= beta):
+            if (nb in list_suspective and random.random() <= beta*get_media_impact(tmp_spread_count)):
                 list_infected.append(nb)
                 list_suspective.remove(nb)
         if(random.random()<=mu):
@@ -100,15 +151,19 @@ while (tmp_spread_count < MAX_Spreading_Time):
     I.append(len(list_infected))
     S.append(len(list_suspective))
     R.append(len(list_recoverd))
+    IR.append(len(list_infected)+len(list_recoverd))
+    logger.info("run/total:%s/%s" % (tmp_spread_count,MAX_Spreading_Time))
 
-plt.plot(x,S,label="S")
+#plt.plot(x,S,label="S")
 plt.plot(x,I,label="I")
-plt.plot(x,R,label="R")
+#plt.plot(x,R,label="R")
+plt.plot(x,IR,label="I+R")
 plt.legend(loc="best", frameon=False)
 plt.xlabel("Time")
 plt.ylabel('Numbers')
 logger.info("S:%s, I:%s, R:%s",len(list_suspective),len(list_infected),len(list_recoverd))
 
 #nx.draw(BA, ps, with_labels=False, node_size=30)
-plt.show()
-#sys.exit()
+plt.savefig((fig % N)+".png", dpi=500, bbox_inches='tight')
+#plt.show()
+sys.exit()
