e4788288fb7fdc587cfa3e26dc4f8ec185e12444
zico <jianjinshu@126.com>
2020-04-02 12:26:14 +0800

    win_20200402_feiyan

--- cn/edu/zju/feiyan/Plot_CasesNewsVsDate.py
+++ cn/edu/zju/feiyan/Plot_CasesNewsVsDate.py
@@ -24,9 +24,9 @@ formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(l
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-data_dir="d://BaiduNetdiskDownload//肺炎相关数据//results//报道-日期.txt"
-fig="d://BaiduNetdiskDownload//肺炎相关数据//figs//CasesNewsVsDates.png"
-fig_cul="d://BaiduNetdiskDownload//肺炎相关数据//figs//CasesNewsVsDates_Culumative.png"
+data_dir="d://BaiduNetdiskDownload//报道-日期.txt"
+fig="d://BaiduNetdiskDownload///figs//CasesNewsVsDates.png"
+fig_cul="d://BaiduNetdiskDownload//figs//CasesNewsVsDates_Culumative.png"
 
 def formatnum(x, pos):
     return '$%.1f$' % (x/10000000)
@@ -48,7 +48,8 @@ while line:
         continue
     words = line.replace('号', '').replace('\n', '').split('\t')
     logger.info(words)
-    dates.append(int(words[0]))
+#    dates.append(int(words[0]))
+    dates.append(line_count-1)
     news_cul.append(int(words[2]))
     if (len(words[3])==0):
         words[3]=0
@@ -71,28 +72,49 @@ print(dates)
 ax1.tick_params(axis='x',labelsize=6)
 
 labels=[]
+labels_show=[]
+show_interval=4
+show_dataes=[]
 num_count=0
+labels_show.append("%s-%s" % (1, 1))
+show_dataes.append(0)
+
 for i in dates:
     num_count+=1
     if(num_count<=31):
         labels.append("%s-%s" % (1,num_count))
-    else:
+        if(num_count % show_interval==0):
+            labels_show.append("%s-%s" % (1, num_count))
+            show_dataes.append(i)
+    elif (num_count <= (31+29)):
         labels.append("%s-%s" % (2, num_count-31))
-plt.xticks(dates,labels)
+        if(num_count % show_interval==0):
+            labels_show.append("%s-%s" % (2, num_count-31))
+            show_dataes.append(i)
+    else:
+        labels.append("%s-%s" % (3, num_count-(31+29)))
+        if (num_count % show_interval == 0):
+            labels_show.append("%s-%s" % (3, num_count-(31+29)))
+            show_dataes.append(i)
+
+plt.xticks(show_dataes,labels_show)
 plt.xlim(1,len(labels))
-time_colors=['gray','coral','khaki','skyblue']
+time_colors=['gray','coral','darkorchid','khaki']
+#time_colors=['gray','coral','khaki','darkorchid']
 trans = mtransforms.blended_transform_factory(ax1.transData, ax1.transAxes)
 
 
 plt.fill_between([1,10], 0, 4000, facecolor=time_colors[0], alpha=0.5, transform=trans)
 plt.fill_between([10,20], 0, 4000, facecolor=time_colors[1], alpha=0.5, transform=trans)
-plt.fill_between([20,len(labels)], 0, 4000, facecolor=time_colors[2], alpha=0.5, transform=trans)
+plt.fill_between([20,60], 0, 4000, facecolor=time_colors[2], alpha=0.5, transform=trans)
+plt.fill_between([60,len(labels)], 0, 4000, facecolor=time_colors[3], alpha=0.5, transform=trans)
 #plt.fill_between([1,10], 0, 4000, facecolor=time_colors[0], alpha=0.5, transform=trans)
 
-num_height=14000
-plt.text(4.5,num_height,"发酵期")
-plt.text(13.5,num_height,"初发期")
-plt.text(26,num_height,"爆发期")
+num_height=50000
+plt.text(1.5,num_height,"发酵期")
+plt.text(11.5,num_height,"初发期")
+plt.text(30,num_height,"爆发期")
+plt.text(70,num_height,"平稳期")
 
 ax2 = ax1.twinx()  # this is the important function
 ax2.set_ylabel('累计信息总数')
@@ -110,10 +132,10 @@ ax1.legend(lns,labs,loc='upper left',
                    fontsize=7, ncol=1, frameon=False)
 #fig.legend(loc='upper left',
 #                   fontsize=7, ncol=1, frameon=False,bbox_to_anchor=(1,1), bbox_transform=ax.transAxes)
-plt.text(len(labels)+0.2,17000000,"$ \\times 10^7$")
+plt.text(len(labels)+0.2,183000000,"$ \\times 10^7$")
 plt.gcf().autofmt_xdate()#自动适应刻度线密度，包括x轴，y轴
 
-ax1.set_title("全国新型肺炎确诊病例与舆情发展趋势图(截止至%s月%s日)" %(2,len(labels)-31))
+ax1.set_title("全国新冠肺炎确诊病例与舆情发展趋势图(截止至%s月%s日)" %(3,len(labels)-31-29))
 
 
 
--- cn/edu/zju/feiyan/mediaplos/mediaspreading.py
+++ cn/edu/zju/feiyan/mediaplos/mediaspreading.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # coding:utf-8
 #COVID-19 spreading
+#SARS-COV-2 spreading
+# python -m cn.edu.zju.feiyan.mediaplos.mediaspreading
 import  pandas  as pd
 import  sys,os
 import logging
@@ -26,7 +28,7 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 data_dir="d://BaiduNetdiskDownload//肺炎相关数据//results//报道-日期.txt"
-fig="..//figs//meida_spreading_sir_%s"
+fig="d://py//cn/edu//zju//feiyan//figs//meida_spreading_sir_N=%s_beta=%.3f_mu=%.1f_alpha=%.1f_lamda=%.1f"
 def formatnum(x, pos):
     return '$%f$' % (x/10000000)
 #formatter = FuncFormatter(formatnum)
@@ -37,29 +39,46 @@ def formatnum(x, pos):
 dates=[]
 cases=[]
 news=[]
+cases_cul=[]
+news_cul=[]
 f = open(data_dir, encoding='UTF-8', mode='r', errors='ignore')
 line = f.readline()
 line_count = 0
 valid_date=16
+
+N = 1000000 #N个节点，默认标号为0到N-1
+m0 = 2  # m0*2 equals average degree
+MAX_Spreading_Time=19
+num_initial_infected=10
+#num_initial_infected=int(N*0.02)
+beta =0.3
+mu=0.5
+alpha=0.2
+lamda=0.7
+
 while line:
     if (line_count <valid_date):
         line_count += 1
         line = f.readline()
         continue
     words = line.replace('号', '').replace('\n', '').split('\t')
-#    logger.info(words)
     dates.append(int(words[0]))
     #news.append(int(words[1]))
     news.append(math.log10(int(words[1])))
     if (len(words[4])==0):
         words[4]=0
     cases.append(int(words[4]))
+    a=int(words[4])
+    b=int(words[3])
+    cases_cul.append(int(words[3]))
     line_count += 1
     line = f.readline()
 f.close()
+#cases.remove(a)
+#cases_cul.remove(b)
 
-alpha=0.2
-lamda=1
+
+
 def get_media_impact(t):
     theta = 0.0
     tmp_count=t #t starts from 0
@@ -73,14 +92,10 @@ def get_media_impact(t):
     #sys.exit()
     return math.exp(-1*theta*alpha)
 
-N = 1000000 #N个节点，默认标号为0到N-1
-m0 = 2  # m0*2 equals average degree
-MAX_Spreading_Time=len(news)
-num_initial_infected=20
-#num_initial_infected=int(N*0.02)
-beta =0.35
-mu=1
 
+
+#logger.info(MAX_Spreading_Time)
+#sys.exit()
 logger.info("Generating network...")
 
 g = nx.barabasi_albert_graph(N, m0)
@@ -131,39 +146,49 @@ S=[]
 I=[]
 R=[]
 IR=[]
-x.append(tmp_spread_count)
-I.append(len(list_infected))
-S.append(len(list_suspective))
-R.append(0)
-IR.append(len(list_infected))
+#x.append(tmp_spread_count)
+#I.append(len(list_infected))
+#S.append(len(list_suspective))
+#R.append(0)
+#IR.append(len(list_infected))
 while (tmp_spread_count <= MAX_Spreading_Time):
+    tmp_num_infected=0
     for infected in list_infected:
         for nb in  list(g.neighbors(infected)):
             if (nb in list_suspective and random.random() <= beta*get_media_impact(tmp_spread_count)):
                 list_infected.append(nb)
                 list_suspective.remove(nb)
+                tmp_num_infected += 1
         if(random.random()<=mu):
             list_infected.remove(infected)
             list_recoverd.append(infected)
     tmp_spread_count += 1
 #    logger.info("time: %s, infected: %s" % (tmp_spread_count,num ))
     x.append(tmp_spread_count)
-    I.append(len(list_infected))
+    I.append(tmp_num_infected)
     S.append(len(list_suspective))
     R.append(len(list_recoverd))
     IR.append(len(list_infected)+len(list_recoverd))
-    logger.info("run/total:%s/%s" % (tmp_spread_count,MAX_Spreading_Time))
-
-#plt.plot(x,S,label="S")
-plt.plot(x,I,label="I")
-#plt.plot(x,R,label="R")
-plt.plot(x,IR,label="I+R")
+    logger.info("run/total:%s/%s" % (tmp_spread_count,MAX_Spreading_Time+1))
+#plt.plot(x,S,label="S")    `
+ax = plt.subplot(221)
+plt.plot(x,I,label="Model")
+plt.plot(x,cases,label="Real")
+plt.ylabel('# of daily infected')
+plt.xlabel("T")
 plt.legend(loc="best", frameon=False)
-plt.xlabel("Time")
-plt.ylabel('Numbers')
+ax = plt.subplot(222)
+plt.plot(x,IR,label="Model")
+plt.plot(x,cases_cul,label="Real")
+plt.ylabel('# of total infected')
+plt.legend(loc="best", frameon=False)
+plt.xlabel("T")
+plt.subplots_adjust(left=None, bottom=None, right=None, top=None,
+                    wspace=0.38, hspace=0.15)
+
 logger.info("S:%s, I:%s, R:%s",len(list_suspective),len(list_infected),len(list_recoverd))
 
 #nx.draw(BA, ps, with_labels=False, node_size=30)
-plt.savefig((fig % N)+".png", dpi=500, bbox_inches='tight')
+plt.savefig((fig % (N, beta,mu,alpha,lamda))+".png", dpi=500, bbox_inches='tight')
 #plt.show()
 sys.exit()
