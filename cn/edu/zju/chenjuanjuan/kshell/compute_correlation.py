03b9c9d64065ce7193a0759c5f285c35c1996cba
zico <jianjinshu@126.com>
2023-03-10 22:38:32 +0800

    chenjuanjuan

--- cn/edu/zju/chenjuanjuan/kshell/compute_correlation.py
+++ cn/edu/zju/chenjuanjuan/kshell/compute_correlation.py
@@ -27,14 +27,9 @@ logger.info("loading data...")
 time_start=time.time()
 df = pd.read_excel(f1)
 df = df.loc[::, ['一层节点数量', '二层节点数量', '三层节点数量', 'Performance']]
-#logger.info(df['id'].value_counts())
-
-#ids = [int(data[i][0]) for i in range(len(data))]
-#ids = sorted(list(set(ids)))
 
 
-
-correlation = pd.DataFrame(columns=['beta', 'correlation'])
+correlation = pd.DataFrame(columns=['beta', 'correlation', 'p-value'])
 
 data = df.values.tolist()
 layer1=[]
@@ -62,19 +57,58 @@ for i in range(len(data)):
     performance.append(float(data[i][3]))
 
 
-beta=-10
+beta=-20.001
 line = 0
-
+step= 10
 x=[]
 y=[]
 
-while (beta < 30):
+'''
+score=[]
+for i in range(len(layer1)):
+    if (layer2[i] == 0):
+        score.append(0)
+    else:
+        score.append(1)
+print(pearsonr(score,performance))
+'''
 
-    score = [1**beta*layer1[i]+2**beta*layer2[i]+3**beta*layer3[i] for i in range(len(layer1))]
+while (beta < 20):
+
+    #score = [math.exp(layer1[i]*beta+layer2[i]*beta*2+layer3[i]*beta*3) for i in range(len(layer1))]
+    #score = [beta**layer1[i]+beta**layer2[i]+beta**layer3[i] for i in range(len(data))]
+
+    score=[]
+    '''
+    for i in range(len(layer1)):
+        if (layer1[i] ==0):
+            score.append(0)
+        else:
+            score.append(layer1[i]**(beta))
+    #score = [layer2[i]**beta for i in range(len(layer1))]
+    '''
+    for i in range(len(layer1)):
+        if (layer1[i] ==0 and layer2[i] ==0):
+            score.append(0)
+        else:
+            s = 0
+            if layer1[i] !=0:
+                s-= layer1[i]**(beta/2)/1.0
+            if layer2[i] !=0:
+                s+= layer2[i]**(beta/2)/1.0
+            if layer3[i] !=0:
+                s+= layer3[i]**(beta/2)/1.0*0
+
+            #score.append((-layer1[i]**(beta)/1.0)+layer2[i]**(beta)/1.0-layer3[i]**beta/1.0)
+            score.append(s)
+#            print(score[i])
+#            exit(0)
     c = pearsonr(score,performance)
-    correlation.loc[line] = [beta, c[0]]
+    correlation.loc[line] = [beta, c[0], c[1]]
+    
     x.append(beta)
     y.append(c[0])
+    
     if abs(beta) <= 0.99:
         step =0.1
     elif abs(beta) <= 10:
@@ -85,15 +119,12 @@ while (beta < 30):
     beta += step
 
 
-plt.semilogx(x,y)
-#plt.plot(x,y)
+#plt.semilogx(x,y)
+plt.plot(x,y)
 plt.xlabel('$ \\beta $')
 plt.ylabel('correlation')
 plt.show()
 
-        #for c in code:
-        #    d = company_code_part.loc[(company_code_part['行业代码'] == 'J')]
-        #    for c
 
 logger.info("writing results to file..")
 correlation.to_csv(f_correlation,index=False)
