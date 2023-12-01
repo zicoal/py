8aa17ccfc24594288983db0986494df9148c9d4a
zico <jianjinshu@126.com>
2023-12-01 21:35:08 +0800

    jonathan_value_network

--- cn/edu/zju/jonathan/valueneworks/computer_edge_weights_csv.py
+++ cn/edu/zju/jonathan/valueneworks/computer_edge_weights_csv.py
@@ -26,7 +26,9 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 q_start_columm=13
-rca_thresholds=[1,1.7,1.8,1.9,2,2.1,2.2,2.3,3]
+rca_thresholds=[1,1.1,1.2,1.3,1.4,1.5,1.6,
+                1.7,1.8,1.9,2,2.1,2.2,2.3,
+                2.4,2.5,2.6,2.7,2.8,2.9,3]
 rca_threshold=2.1
 
 #equations = "score1-5"
