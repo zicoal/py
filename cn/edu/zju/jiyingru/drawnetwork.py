40849f60ced1dcf1e057daa1d4507fdbd65f1948
zico <jianjinshu@126.com>
2023-12-14 16:07:27 +0800

    jiyingru

--- cn/edu/zju/jiyingru/drawnetwork.py
+++ cn/edu/zju/jiyingru/drawnetwork.py
@@ -1,5 +1,5 @@
 import matplotlib
-matplotlib.use('TkAgg')
+matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontManager
 from pylab import mpl
@@ -11,6 +11,7 @@ import pandas as pd
 from random import *
 import time
 import logging
+from matplotlib.font_manager import FontProperties
 import xlsxwriter
 from openpyxl import load_workbook
 from xlsxwriter import Workbook
@@ -29,19 +30,16 @@ logger.addHandler(ch)
 
 rca_threshold=1.8
 
-#font_path=r"c:\windows\fonts\simsun.ttc"
-#font = FontProperties(fname=font_path, size=10)
 
-mpl_fonts = set(f.name for f in FontManager().ttflist)
-matplotlib.rc("font", family='SimSun')
-
-#print('all font list get from matplotlib.font_manager:')
-#for f in sorted(mpl_fonts):
-#    print('\t' + f)
-rca_thresholds = [1,2]
+matplotlib.rcParams['font.sans-serif'] = ['Arial']
+rca_thresholds = [1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6,
+                  1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3,
+                  2.4,2.5,2.6,2.7,2.8,2.9,3]
 #equations = "score1-5"
-question_type = "data1"
-questions = [
+data_types =["data1",'data2']
+data_files = ['ESGstudy1Data','ESGstudy2Data']
+questions =[
+        [
          'Q3_1','Q3_2','Q3_3','Q3_4','Q4_1','Q4_2','Q4_3','Q4_4','Q4_5',
          'Q5_1','Q5_2','Q5_3','Q5_4','Q5_5','Q6_1','Q6_2','Q6_3',
          'Q7_1','Q7_2','Q7_3','Q7_4','Q7_5','Q7_6','Q7_7','Q7_8	',
@@ -51,59 +49,74 @@ questions = [
          'Q12_1','Q12_2','Q12_3','Q13_1','Q13_2','Q13_3','Q13_4','Q13_5','Q13_6','Q13_7',
          'Q14_1','Q14_2','Q14_3','Q14_4','Q14_5','Q14_6','Q14_7','Q14_8','Q14_9',
          'Q15_1','Q15_2','Q15_3','Q15_4','Q15_5','Q15_6',
-         'Q16_1','Q16_2','Q16_3','Q16_4','Q16_5','Q16_6','Q16_7','Q17']
-
+         'Q16_1','Q16_2','Q16_3','Q16_4','Q16_5','Q16_6','Q16_7','Q17'],
+        [
+          'Q1_1','Q1_2','Q1_3','Q1_4','Q2_1','Q2_2','Q2_3','Q2_4','Q2_5','Q2_6','Q2_7',
+          'Q3_1','Q3_2','Q3_3','Q3_4','Q3_5','Q3_6','Q3_7','Q3_8','Q3_9','Q3_10','Q3_11','Q3_12','Q3_13',
+          'Q4_1','Q4_2	Q4_3','Q4_4','Q5_1','Q5_2','Q5_3','Q5_4','Q5_5','Q5_6',
+          'Q6_1','Q6_2','Q6_3','Q6_4','Q6_5','Q6_6','Q7_1','Q7_2','Q7_3','Q7_4','Q7_5','Q7_6','Q7_7','Q7_8','Q7_9',
+          'Q8_1','Q8_2','Q8_3','Q8_4','Q8_5','Q8_6','Q9_1','Q9_2','Q9_3','Q9_4','Q10_1','Q10_2']
+        ]
 edge_weight_manipulte = 3
+for m in range(len(data_types)):
 
-for rca_threshold in rca_thresholds:
-    f_weights = 'D:\\pydata\\data\\jiyingru\\%s\\weights_number_min_%.1f.csv' % (question_type, rca_threshold)
-    f_fig = 'D:\\pydata\\data\\jiyingru\\%s\\figs\\fig_%.1f.png' % (question_type, rca_threshold)
-    #f_code= 'D:\\py\\data\\jonathan\\weights_number_min_%.1f.xlsx' % rca_threshold
+    for rca_threshold in rca_thresholds:
 
-    logger.info("RCA:%.1f,Equation:%s" % (rca_threshold,question_type))
+        fig = plt.figure(figsize=(5,5))
 
-    logger.info("loading data...")
-    time_start=time.time()
+        f_weights = 'D:\\pydata\\data\\jiyingru\\%s\\weights_number_min_%.1f.csv' % (data_types[m], rca_threshold)
+        f_fig = 'D:\\pydata\\data\\jiyingru\\%s\\figs\\fig_%.1f.png' % (data_types[m], rca_threshold)
 
-    df= pd.read_csv(f_weights)
+        logger.info("Data:%s,RCA:%.1f" % (data_types[m],rca_threshold))
 
-    country_quest_phi_weight_network = df.loc[::, ['QA', 'QB', 'weights']]
-    time_end = time.time()
+        logger.info("loading data...")
+        time_start=time.time()
 
-    logger.info("data loaded...time:%d s'", time_end - time_start)
+        df= pd.read_csv(f_weights)
+
+        country_quest_phi_weight_network = df.loc[::, ['QA', 'QB', 'weights']]
+        time_end = time.time()
+
+        logger.info("data loaded...time:%d s'", time_end - time_start)
 
 
-    #
-    #rows = 2
-    #cols = 2
-    #k=0
+        #
+        #rows = 2
+        #cols = 2
+        #k=0
 
-     #   k+=1
+         #   k+=1
 
-    data = df.values.tolist()
+        data = df.values.tolist()
 
-    g = nx.Graph()
-    for i in range(len(data)):
-        g.add_edge(data[i][0], data[i][1], weight=data[i][2])
-    logger.info('Questions:%d,nodes/edges:%d/%d, time:%d s',  len(questions),len(g.nodes), len(g.edges),time_end - time_start)
-    #plt.subplot(rows, cols, k)
+        g = nx.Graph()
+        for i in range(len(data)):
+            g.add_edge(data[i][0], data[i][1], weight=data[i][2])
+        logger.info('Questions:%d,nodes/edges:%d/%d, time:%d s',  len(questions[m]),len(g.nodes), len(g.edges),time_end - time_start)
+        #plt.subplot(rows, cols, k)
 
-    edgewidth = [g.get_edge_data(*e)['weight']*edge_weight_manipulte for e in g.edges()]
+        edgewidth = [g.get_edge_data(*e)['weight']*edge_weight_manipulte for e in g.edges()]
 
- #   plt.xlabel('\u5e73\u5747\u503c')
+     #   plt.xlabel('\u5e73\u5747\u503c')
 
-#   plt.rcParams['font.family'] = 'SimSun'
-    nx.draw(g,pos=nx.spring_layout(g),
-            node_color='r',
-            edge_color='b',
-            with_labels = True,
-            node_size = 150,
-            font_size=3,
-            width =edgewidth )
+    #   plt.rcParams['font.family'] = 'SimSun'
+        nx.draw(g,pos=nx.spring_layout(g),
+                node_color='r',
+                edge_color='b',
+                with_labels = True,
+                node_size = 150,
+                font_size=3,
+                width =edgewidth )
 
-#    mpl.rcParams['font.']=['times-new-roman']
-   #plt.title("rca = %.1f" % rca_threshold)
-    #    plt.axis("off")
-   # plt.show()
-    plt.savefig(f_fig,dpi=800)
+    #    mpl.rcParams['font.']=['times-new-roman']
+        #plt.title("RCA = %.1f" % rca_threshold)
+        # plt.title("ss", fontsize=100)
+        fig.suptitle(f"RCA = {rca_threshold:.1f}")
+        # plt.axis("off")
+
+        # plt.show()
+        # plt.rcParams['font.sans-serif'] = ['SimHei']
+        # plt.rcParams['axes.unicode_minus'] = False
+        # plt.savefig(f'./{rca_threshold}.png', dpi=800)
+        plt.savefig(f_fig, dpi=800)
 
